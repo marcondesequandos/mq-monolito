59f58832a40e7cce2430e58189f41c13fdcb1648
felipe.carvalho.glo <felipe.carvalho@globalhitss.com.br>
2024-01-12 14:06:00 -0300

    Iniciando criação de API

--- src/modules/infrastructure/product-registration/product.repository.ts
+++ src/modules/infrastructure/product-registration/product.repository.ts
@@ -1,5 +1,6 @@
 import Id from "../../@shared/domain/value-object/id.value-object";
-import Product from "../domain/product.entity";
+import Product from "../../product-adm/domain/product.entity";
+
 import ProductGateway from "../gateway/product.gateway";
 import { ProductModel } from "./product.model";
 
